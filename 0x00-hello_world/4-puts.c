dee6b94eba543ee1d15af6d2d0b70dc6e2586635
AbnzrM <abeni350@gmail.com>
2022-04-07 15:25:51 +0300

    hello world in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is a description of main
+ *
+ * Return: It returns zero
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
