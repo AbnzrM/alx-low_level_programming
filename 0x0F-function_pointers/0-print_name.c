dc0c30171c410c76c16ebc122f307543ee6c2ec5
AbnzrM <abeni350@gmail.com>
2022-05-11 16:41:21 +0300

    the pointer function commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
 /**
- * print_name - Function to call a function
- *
- * @name: Parameter 1
- * @f: parameter function
+ * print_name - prints the name
+ * @name: name
+ * @f: function
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
-	{
-		if (name != NULL)
-		{
-			(*f)(name);
-		}
-	}
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }
+
